f44219b64bc5f3eb504039e879cb600a8779825a
Francisco <6652831+franmastromarino@users.noreply.github.com>
2023-12-31 09:50:45 +0100

    Update template wordpress version

--- src/template/plugin-init.php
+++ src/template/plugin-init.php
@@ -11,10 +11,10 @@
  * License:                 GPLv3
  * Domain Path:             /languages
  * Request at least:        4.7.0
- * Tested up to:            6.2
+ * Tested up to:            6.4
  * Requires PHP:            5.6
  * WC requires at least:    4.0
- * WC tested up to:         8.2
+ * WC tested up to:         8.3
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
